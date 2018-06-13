db884591ee8a6c5fe8420b0073bbb5a1aa022e18
pd111 <40251582+pd111@users.noreply.github.com>
2018-06-13 18:52:59 -0500

    Create mytest.bash
    Initial

---
+++ mytest.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "This is my first bash script"
+echo ""
+
+echo "Max: " > summary.txt
+cat cars.csv | tail -n+3 | cut -d ";" -f1,2 | sort -gk 2,2 -t ";"| tail -n 1 >> summary.txt
+echo "Min: " >> summary.txt
+cat cars.csv | tail -n+3 | cut -d ";" -f1,2 | sort -gk 2,2 -t ";"| head -n 8 >> summary.txt
+Echo "List of countries of origin:" >> summary.txt
+cat cars.csv | cut -d ";" -f9 | sort -u >> summary.txt
